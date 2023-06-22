4b78840982b3c272f3df5fd30102da44b0c357bd
Jigar Gangani <jgangani6887@conestogac.on.ca>
2023-06-22 19:58:06 +0530

    feat : test cases

---
+++ PixelCalc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pixelcalc
+{
+    public static class PixelCalc
+    {
+        public static double CalculatePPI(int width, int height, int diagonal)
+        {
+            double diagonalResolution = Math.Sqrt(Math.Pow(width, 2) + Math.Pow(height, 2));
+            return diagonalResolution / diagonal;
+        }
+
+        public static double CalculateDiagonalInPixels(int width, int height)
+        {
+            return Math.Sqrt(Math.Pow(width, 2) + Math.Pow(height, 2));
+        }
+
+        public static double CalculateDotPitch(int width, int height, int diagonal)
+        {
+            return 25.4 / CalculatePPI(width, height, diagonal);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Pixelcalc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Enter Screen dimensions");
+            Console.WriteLine("2. Exit");
+
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                Console.Write("Enter screen width: ");
+                int width = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter screen height: ");
+                int height = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter diagonal size: ");
+                int diagonal = int.Parse(Console.ReadLine());
+
+                double ppi = PixelCalc.CalculatePPI(width, height, diagonal);
+                double diagonalPixels = PixelCalc.CalculateDiagonalInPixels(width, height);
+                double dotPitch = PixelCalc.CalculateDotPitch(width, height, diagonal);
+
+                Console.WriteLine($"PPI: {ppi}");
+                Console.WriteLine($"Diagonal in pixels: {diagonalPixels}");
+                Console.WriteLine($"Dot Pitch: {dotPitch}");
+            }
+            else if (input == "2")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+        }
+    }
+}
